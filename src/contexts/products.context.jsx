f0b4df45714ce692ebebbed44d316dc0433293d6
Bruce Tang <bruce@brucetangdesign.com>
2024-02-19 17:27:40 -0500

    Added categories to firebase, created method to get categoryMap

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,18 +1,26 @@
-import { createContext, useState, useEffect } from "react"
+import { createContext, useState, useEffect } from "react";
+import {
+  getCategoriesAndDocuments,
+  }
+  from "../utils/firebase/firebase.utils";
 
-import PRODUCTS from '../shop-data.json';
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({children}) => {
-  const [products, setProducts ] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
   const value = {products};
 
-  /*useEffect(() =>{
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    }
 
-  },[]);*/
+    getCategoriesMap();
+  }, []);
 
   return (
     <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
