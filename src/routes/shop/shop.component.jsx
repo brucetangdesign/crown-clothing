c6e11dd5b30217f822f4fd6802344afae185364c
Bruce Tang <bruce@brucetangdesign.com>
2024-02-20 22:15:48 -0500

    Refactored product context to categories context, Set up shop page using categories

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,18 +1,33 @@
-import { useContext } from "react";
-import { ProductsContext } from "../../contexts/products.context";
+import { useContext, Fragment } from "react";
+import { CategoriesContext } from "../../contexts/categories.context";
 import ProductCard from "../../components/product-card/product-card.component";
 
 import './shop.styles.scss';
 
 const Shop = () => {
-    const {products} = useContext(ProductsContext);
+    const { categoriesMap } = useContext(CategoriesContext);
+    const toTitleCase = (word) => {
+        return word.split(' ').map(letters => (letters.charAt(0).toUpperCase() + letters.slice(1)));
+    }
 
     return(
-        <div className='products-container'>
-            {products.map((product)=> (
-                <ProductCard key={product.id} product={product}/>
-            ))}
-        </div>
+        <Fragment>
+            {
+                Object.keys(categoriesMap).map((title) => (
+                    <Fragment key={categoriesMap[title].id}>
+                        <h2>{toTitleCase(title)}</h2>
+                        <div className='products-container'>
+                            {
+                                categoriesMap[title].map((product, index) => (
+                                    index < 4 ? <ProductCard key={product.id} product={product} /> : null
+                                ))
+                            }
+                        </div>
+                    </Fragment>
+                )
+            )}
+        
+        </Fragment>
     );
 }
 
