3c707e4ed16304366e88e81653f856a24ec184a5
Bruce Tang <bruce@brucetangdesign.com>
2024-02-22 17:57:13 -0500

    Created category preview and categories preview components, set up nested routing

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,33 +1,16 @@
-import { useContext, Fragment } from "react";
-import { CategoriesContext } from "../../contexts/categories.context";
-import ProductCard from "../../components/product-card/product-card.component";
+import { Routes, Route } from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 
 import './shop.styles.scss';
 
 const Shop = () => {
-    const { categoriesMap } = useContext(CategoriesContext);
-    const toTitleCase = (word) => {
-        return word.split(' ').map(letters => (letters.charAt(0).toUpperCase() + letters.slice(1)));
-    }
 
     return(
-        <Fragment>
-            {
-                Object.keys(categoriesMap).map((title) => (
-                    <Fragment key={categoriesMap[title].id}>
-                        <h2>{toTitleCase(title)}</h2>
-                        <div className='products-container'>
-                            {
-                                categoriesMap[title].map((product, index) => (
-                                    index < 4 ? <ProductCard key={product.id} product={product} /> : null
-                                ))
-                            }
-                        </div>
-                    </Fragment>
-                )
-            )}
-        
-        </Fragment>
+        <Routes>
+            <Route index element={<CategoriesPreview/>} />
+            <Route path=":category" element={<Category />} />
+        </Routes>
     );
 }
 
